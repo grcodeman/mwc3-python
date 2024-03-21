b51ee28c3068714f052f4481cedf77b9d7e1cb26
khangnguyen0106 <dsy1090@wmich.edu>
2024-03-21 14:34:05 -0400

    deleting book_data, add other options

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def main():
     while inputStop == False:
         inputMode = int(input("Select what you want to do (1 - add a book manually, 2 - import a .csv file, 3 - export csv, 4 - export database): "))
         if inputMode == 1:
-            manualInput()
+            bookData = manualInput()
         elif inputMode == 2:
+            break
+        elif inputMode == 3:
+            break
+        elif inputMode == 4:
             break
         
 def manualInput():
@@ -20,22 +24,6 @@ def manualInput():
         headerData.append(userInput)
     book = Book(*headerData)  # Unpack headerData to pass individual attributes
     books.append(book)
-    books_data = []
-    for book in books:
-        books_data.append({
-            "book_id": book.book_id,
-            "title": book.title,
-            "authors": book.authors,
-            "average_rating": book.avg_rating,
-            "isbn": book.isbn,
-            "isbn13": book.isbn13,
-            "language_code": book.language_code,
-            "num_pages": book.num_pages,
-            "ratings_count": book.ratings_count,
-            "text_reviews_count": book.text_reviews_count,
-            "publication_date": book.publication_date,
-            "publisher": book.publisher
-        })
 
 if __name__ == "__main__":
     main()
