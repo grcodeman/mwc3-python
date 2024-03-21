b64bc2be3015f160b4c07878cc6797863e94e6ab
Cody <68598045+grcodeman@users.noreply.github.com>
2024-03-21 15:00:57 -0400

    Update csvtool.py

--- csvtool.py
+++ csvtool.py
@@ -3,10 +3,13 @@ import csv
 
 # headers = ["bookID","title","authors","average_rating","isbn","isbn13","language_code", "num_pages","ratings_count","text_reviews_count","publication_date","publisher"]
 
-# this file will contain converting csv, updating dataframe and then exporting csv
+# this file will contain converting csv, updating datafram and then exporting csv
+
+test_csv = pd.DataFrame(columns=["bookID","title","authors","average_rating","isbn","isbn13","language_code", "  num_pages","ratings_count","text_reviews_count","publication_date","publisher"])
+
 
 # convert csv to dataframe
-def convert_csv(path):
+def convert_csv(df, path):
     # uses the traditional csv method to get the csv headers and removes any empty strings
     f = open('data/books.csv', 'r')
     reader = csv.reader(f)
@@ -14,12 +17,15 @@ def convert_csv(path):
     headers.remove("")
 
     # uses the pandas package to convert to a dataframe
-    df = pd.read_csv(path, usecols=headers)
+    new = pd.read_csv(path, usecols=headers)
+
+    df = pd.concat([df, new], ignore_index=True)
     print(df.columns)
     print(df.head(3))
     return df
 
-test_csv = convert_csv("data/books.csv")
+#test_csv = convert_csv(test_csv, "data/books.csv")
+#print(test_csv.columns)
 
 # adds a line to the dataframe
 def add_row(df, inputs):
@@ -28,11 +34,12 @@ def add_row(df, inputs):
 
     return df
 
-test_csv = add_row(test_csv, ["211738","bruh","test","average_rating","isbn","isbn13","language_code", "num_pages","ratings_count","text_reviews_count","publication_date","publisher"])
+
+#test_csv = add_row(test_csv, ["211738","bruh","test","average_rating","isbn","isbn13","language_code", "num_pages","ratings_count","text_reviews_count","publication_date","publisher"])
 
 # exports dataframe to a csv in output folder
 def output_csv(df):
     new = df.to_csv("output/test.csv", sep=",", index=False)
     return new
 
-output_csv(test_csv)
+#output_csv(test_csv)
